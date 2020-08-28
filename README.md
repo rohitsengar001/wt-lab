8bf3b9a52a4072c74656e7c96c5f7b069975293a
rohitsengar001 <rohitsengar001@gmail.com>
2020-08-28 18:36:06 +0530

    add header file in index.html

--- README.md
+++
@@ -1,2 +0,0 @@
-# wt-lab
-site already published visit now: https://rohitsengar001.github.io/wt-lab/
---
+++ first.js
@@ -0,0 +1,6 @@
+myFunction()
+{
+    var message="this is local variable";
+    document.getElementById().innerHTML="local scope have changed this line";
+    
+}
