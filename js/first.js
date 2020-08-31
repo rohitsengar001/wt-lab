a47a97fc052b4130ae4fe7afaaaf0a602a10124c
rohitsengar001 <rohitsengar001@gmail.com>
2020-09-01 00:26:03 +0530

    making fun for add two Numbers

--- js/first.js
+++ js/first.js
@@ -5,3 +5,11 @@ function myFunction()
     document.getElementById("para").innerHTML=message;
     
 }
+
+function addTwoNumber()
+{
+    let n1= Number(window.prompt("Enter the first number"));
+    let n2=Number(window.prompt("enter the second number"));
+    let sum = n1 + n2;
+    document.getElementById("sum").innerHTML="sum is :"+sum;
+}
