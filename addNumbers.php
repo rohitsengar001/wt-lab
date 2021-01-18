3b6633b9150046925b2f2ed2a90c436c034f726b
rohitsengar001 <rohitsengar001@gmail.com>
2021-01-18 19:09:50 +0530

    calculator added

---
+++ addNumbers.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>addtwoNumber</title>
+</head>
+<body>
+    <?php
+       $num1=10;
+       $num2=90;
+       echo "$sum=$num1 + $num2";
+    ?>   
+</body>
+</html>
